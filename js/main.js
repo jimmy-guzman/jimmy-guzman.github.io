cf1e155610ec8b7f39ce5969f94fbdd3ceade9c1
Jimmy Guzman <jimmy.guzman@outlook.com>
2018-03-21 18:47:01 -0500

    added smooth scroll

--- js/main.js
+++ js/main.js
@@ -13,3 +13,42 @@ function fixNav() {
 
 window.addEventListener("scroll", fixNav);
 
+document.addEventListener("click", (e) => {
+  if (e.target.className === "nav__link") {
+    e.target.classList.remove("selected");
+    document.querySelector(e.target.dataset.href).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  }
+});
+
+(function() {
+
+
+  var section = document.querySelectorAll(".section");
+  var sections = {};
+  var i = 0;
+
+  Array.prototype.forEach.call(section, function(e) {
+    sections[e.id] = e.offsetTop;
+  });
+
+  console.log(sections);
+
+  window.onscroll = function() {
+    var scrollPosition = document.documentElement.scrollTop || document.body.scrollTop;
+
+    for (i in sections) {
+      if (sections[i] <= scrollPosition) {
+        document.querySelectorAll(`[data-href]`).forEach(link => {
+         
+          if(link.dataset.href.includes(i)) {
+            console.log(link.dataset.href);
+            link.classList.add("selected");
+          }
+        })
+      }
+    }
+  };
+})();
